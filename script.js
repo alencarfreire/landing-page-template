37b962cbd55a20cef063e24ebea378c481c6745c
Vinícius de Alencar Freire <alencarfreire@gmail.com>
2024-09-21 19:40:36 -0300

    add animations

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+function initAccordionList() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
+  function activeAccordion() {
+    const activeclass = "ativo";
+    this.classList.toggle(activeclass);
+    this.nextElementSibling.classList.toggle(activeclass);
+    this.childNodes[1].classList.toggle(activeclass);
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
+  });
+}
+
+initAccordionList();
+
+function initiAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowsMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowsMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+  }
+
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+
+initiAnimacaoScroll();
